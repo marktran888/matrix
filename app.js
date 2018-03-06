b7b9bed52e97755159057b4c3ffdee133b08369a
Mark Tran <mark.tran@hotmail.co.uk>
2018-03-06 23:21:35 +0000

    show single rain

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log('test');
+window.addEventListener('DOMContentLoaded', () => {
+  const rain = document.querySelector('.rain');
+
+  function showRain(){
+    return String.fromCharCode(0x30A0 + Math.round(Math.random()*96));
+  }
+
+  rain.innerHTML = showRain();
+
+});
