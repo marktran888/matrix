14208150f9d46f3d65c71ee41ae6e6b072c5d955
Mark Tran <mark.tran@hotmail.co.uk>
2018-03-07 00:03:42 +0000

    stream of rain fall

--- app.js
+++ app.js
@@ -5,6 +5,15 @@ window.addEventListener('DOMContentLoaded', () => {
     return String.fromCharCode(0x30A0 + Math.round(Math.random()*96));
   }
 
-  rain.innerHTML = showRain();
+  function stream(){
+    const streamLength = Math.floor(Math.random()*20);
+    let stream ='';
+    for (let i = 0; i < streamLength; i++) {
+      stream += showRain();
+    }
+    return stream;
+  }
+
+  rain.innerHTML = stream();
 
 });
